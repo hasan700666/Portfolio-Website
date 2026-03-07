c526f1bd7860a36d604f2b2abd0dab07f49344bf
Mohammod Hasan Al Muttaki <hasanalmuttaki@gmail.com>
2026-03-07 12:45:04 +0600

    added the all section on app.jsx file and Intricate tailwind css on this projects

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,26 @@
-// Hero section component (place your profile image in src/assets/profile.jpg)
-import Hero from './components/Home';
-
-import './App.css';
+import "./App.css";
+import About from "./components/About/About";
+import Contact from "./components/Contact/Contact";
+import Education from "./components/Education/Education";
+import Experience from "./components/Experience/Experience";
+import Footer from "./components/Footer/Footer";
+import Home from "./components/Home/Home";
+import Navbar from "./components/Navbar/Navbar";
+import Projects from "./components/Projects/Projects";
+import Skills from "./components/Skills/Skills";
 
 function App() {
   return (
     <>
-      <Hero />
+      <Navbar></Navbar>
+      <Home></Home>
+      <About></About>
+      <Skills></Skills>
+      <Education></Education>
+      <Experience></Experience>
+      <Projects></Projects>
+      <Contact></Contact>
+      <Footer></Footer>
     </>
   );
 }
