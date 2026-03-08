869a70d6a3fda8deec7cb3fbf86eddf7e8e64072
Mohammod Hasan Al Muttaki <hasanalmuttaki@gmail.com>
2026-03-09 01:41:53 +0600

    add some text on home page

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import img from "../../assets/Adobe Express.png";
 import "./Home.css";
+import { FaArrowDownLong } from "react-icons/fa6";
 
 const Home = () => {
   return (
@@ -11,10 +12,21 @@ const Home = () => {
         <span className="tag-line">FULL-STACK MERN DEVELOPER</span>
         <span className="tag-line">FULL-STACK MERN DEVELOPER</span>
       </div>
-      <div className="tag-image">
-        {" "}
-        {/* ← changed from image-container */}
-        <img src={img} alt="" />
+      <div>
+        <div className="m-10 mt-40 text-2xl">
+          <div>A Web Developer</div>
+          <div>MERN Stack</div>
+        </div>
+        <div className="tag-image">
+          {" "}
+          <img src={img} alt="" />
+        </div>
+        <div className="m-10 mt-70 text-2xl text-end">
+          <div>Scroll Down</div>
+          <div className="flex justify-end mt-3">
+            <FaArrowDownLong  />
+          </div>
+        </div>
       </div>
     </div>
   );
