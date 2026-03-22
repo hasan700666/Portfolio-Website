ea136a8b0f5495a29aeff03431632d8b54d8ed17
Mohammod Hasan Al Muttaki <hasanalmuttaki@gmail.com>
2026-03-23 00:13:44 +0600

    added automatic scroll logic by footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,22 +1,36 @@
-import React from 'react';
-import './Footer.css';
+import React from "react";
+import "./Footer.css";
 
 const Footer = () => {
-    return (
-        <div className='mt-30 h-72 overflow-hidden'>
-            <div className="flex justify-between items-center mx-90 ">
-                <a href="#home" className='hover:underline'>Home</a>
-                <a href="#about" className='hover:underline'>About</a>
-                <a href="#skills" className='hover:underline'>Skills</a>
-                <a href="#education" className='hover:underline'>Education</a>
-                <a href="#experience" className='hover:underline'>Experience</a>
-                <a href="#projects" className='hover:underline'>Projects</a>
-            </div>
-            <div className=''>
-                <div className='text-center text-black teg-line'>CRAFTING CLEAN CODE</div>
-            </div>
+  return (
+    <div className="mt-30 h-72 overflow-hidden">
+      <div className="flex justify-between items-center mx-90 ">
+        <a href="#home" className="hover:underline">
+          Home
+        </a>
+        <a href="#about" className="hover:underline">
+          About
+        </a>
+        <a href="#education" className="hover:underline">
+          Education
+        </a>
+        <a href="#skills" className="hover:underline">
+          Skills
+        </a>
+        <a href="#projects" className="hover:underline">
+          Projects
+        </a>
+        <a href="#contact" className="hover:underline">
+          Contact
+        </a>
+      </div>
+      <div className="">
+        <div className="text-center text-black teg-line">
+          CRAFTING CLEAN CODE
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
